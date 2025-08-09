21fed773df569470e8d01cab176641ca8cd3e51f
rajputnishant29 <nishantrajput7017@gmail.com>
2025-08-09 14:06:00 +0530

    Bug: fixed for FGP

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const auth = require('../middlewares/auth');
 const sendMail = require('../utils/sendMail');
-const sendMail = require('../utils/sendMail');
 
 const router = express.Router();
 
