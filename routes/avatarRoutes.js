ce7b349204591c6bcacfdc4fd4d1f1929f8a5795
rajputnishant29 <rajputnishant2005@gmail.com>
2026-09-14 02:58:08 +0530

    update: user_ avatars

--- routes/avatarRoutes.js
+++ routes/avatarRoutes.js
@@ -12,6 +12,23 @@ const AVATAR_LIST = [
   `${BASE_URL}/avatar_05.png`,
   `${BASE_URL}/avatar_06.png`,
   `${BASE_URL}/avatar_07.png`,
+  `${BASE_URL}/avatar_08.png`,
+  `${BASE_URL}/avatar_09.png`,
+  `${BASE_URL}/avatar_10.png`,
+  `${BASE_URL}/avatar_11.png`,
+  `${BASE_URL}/avatar_12.png`,
+  `${BASE_URL}/avatar_13.png`,
+  `${BASE_URL}/avatar_14.png`,
+  `${BASE_URL}/avatar_15.png`,
+  `${BASE_URL}/avatar_16.png`,
+  `${BASE_URL}/avatar_17.png`,
+  `${BASE_URL}/avatar_18.png`,
+  `${BASE_URL}/avatar_19.png`,
+  `${BASE_URL}/avatar_20.png`,
+  '${BASE_URL}/avatar_21.png',
+  `${BASE_URL}/avatar_22.png`,
+  `${BASE_URL}/avatar_23.png`,
+  `${BASE_URL}/avatar_24.png`,
 ];
 
 router.get('/', (req, res) => {
